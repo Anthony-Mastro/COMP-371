3cb8a4739617da1023d278b442cf3b6a9f076b93
Anthony-Mastro <74116485+Anthony-Mastro@users.noreply.github.com>
2026-07-14 10:54:38 -0400

    created branch and uploaded test file

---
+++ Assignment1/assignment1.cpp
@@ -0,0 +1,11 @@
+//
+// Created by anthony M on 2026-07-14.
+//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, COMP 371!" << std::endl;
+    std::cout << "Project is set up successfully." << std::endl;
+
+    return 0;
+}
